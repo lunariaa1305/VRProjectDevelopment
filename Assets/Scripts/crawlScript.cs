c4a6ae4c6532f8a6c73219c7a0221204d10e6c43
Prismatic-Echo <alx75730@gmail.com>
2026-04-17 12:27:59 +0100

    Fix: wall clipping fix attempt
    As stated

--- Assets/Scripts/crawlScript.cs
+++ Assets/Scripts/crawlScript.cs
@@ -1,13 +1,15 @@
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class crawlScript : MonoBehaviour
 {
 
     Vector3 startRControllerPosition, currentRControllerPosition, startLControllerPosition, currentLControllerPosition, startTransformPosition;
+    public Vector3 movementVector;
     float lButtonPress, rButtonPress, XTransform, YTransform, ZTransform;
     public float speedModifier, movementSmoothing;
-    bool moveWithRController, moveWithLController = false;
+    bool moveWithRController, moveWithLController, interrupt = false;
 
     public LayerMask maskForWalls;
 
@@ -22,7 +24,7 @@ public class crawlScript : MonoBehaviour
     {
         rButtonPress = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
         lButtonPress = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
-        if (rButtonPress > 0.1f)
+        if (rButtonPress > 0.1f && !interrupt) // Maybe check to see if anything should interrupt the dragging? So we are able to apply physics and collisions
         {
             // If not already moving with the controller, set a start position
             if (!moveWithRController)
@@ -38,7 +40,7 @@ public class crawlScript : MonoBehaviour
             moveWithRController = false;
         }
 
-        if (lButtonPress > 0.1f)
+        if (lButtonPress > 0.1f && !interrupt)
         {
             // If not already moving with the controller, set a start position
             if (!moveWithLController)
@@ -76,9 +78,33 @@ public class crawlScript : MonoBehaviour
             ZTransform = (startLControllerPosition.z - currentLControllerPosition.z) * speedModifier;
         }
 
+
         if ((moveWithLController || moveWithRController)) // Potential fix for wall clipping
         {
-            transform.position = new Vector3(startTransformPosition.x + XTransform, startTransformPosition.y + YTransform, startTransformPosition.z + ZTransform); // Potential improvement to Y movement
+            movementVector = new Vector3(startTransformPosition.x + XTransform, startTransformPosition.y + YTransform, startTransformPosition.z + ZTransform);
+            Debug.DrawRay(transform.position, new Vector3(), Color.pink, 20f);
+            transform.position = movementVector; // Potential improvement to Y movement
+            
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        while (collision.gameObject.layer == 3)
+        {
+            interrupt = true;
+            //transform.position = new Vector3(-movementVector.x, movementVector.y, -movementVector.z);
+        }
+
+        if (collision.gameObject.layer != 3)
+        {
+            interrupt = false;
         }
     }
 }
+
+
+/*
+ * 
+ *  https://developers.meta.com/horizon/documentation/unity/unity-isdk-ray-interaction/
+ * 
+ */
