10417cc7d02e55ff098a3d92eed7dc65c42a46fb
THATguyofguys <LUCA.COZIER-SILVERA23@bathspa.ac.uk>
2026-05-06 16:56:53 +0100

    WHAT'S WRONG
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -2,8 +2,8 @@ using UnityEngine.Audio;
 using UnityEngine;
 
 [System.Serializable]
-public class Sound {
-    
+public class Sound
+{
     public string name;
 
     public AudioClip clip;
@@ -15,5 +15,5 @@ public class Sound {
 
     [HideInInspector]
     public AudioSource source;
-
 }
+
